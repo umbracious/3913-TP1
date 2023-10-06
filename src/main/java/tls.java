e22114a78b38b62a1d268f6e167ceb597837b974
umbracious <63139410+umbracious@users.noreply.github.com>
2023-10-06 13:22:30 -0400

    error handling

--- src/main/java/tls.java
+++ src/main/java/tls.java
@@ -9,32 +9,41 @@ public class tls {
         // Only 1 argument, prints to command line if valid path
         if (args.length == 1){
             String path = args[0];
+            File directory = new File(path);
+
+            if (!directory.exists() || !directory.isDirectory()){
+                System.err.println("Incorrect directory. Usage: java <entry-dir>");
+                System.exit(1);
+            }
 
         }
 
         // 2 arguments and first argument is "-o", output to .csv file in same directory
         else if (args.length == 2 && args[0]=="-o"){
             String path = args[1];
+            File directory = new File(path);
 
         }
 
         // 3 arguments and first argument is "-o", output to .csv file in specified directory
         else if (args.length == 3 && args[0]=="-o"){
             String path = args[2];
+            File directory = new File(path);
 
         }
 
         // Invalid argument
         else {
-            System.err.println("Usage correcte: java <chemin-entree> || java -o (<chemin-sortie>) <chemin-entree>");
+            System.err.println("Correct usage: java <entry-dir> || java -o (<output-dir>) <entry-dir>");
             System.exit(1);
         }
 
     }
 
     // Find every test java file within the directory and its sub-directories
-    public static List<File> testFiles(String path){
-
+    public static List<File> testFiles(File directory){
+        List<File> javaFiles = new ArrayList<>();
+        
     }
 
 }
