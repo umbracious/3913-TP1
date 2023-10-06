7fe14b46c3fee0fb7791bf82aa3013e8b3fed55a
umbracious <63139410+umbracious@users.noreply.github.com>
2023-10-06 13:02:26 -0400

    find path form args

--- src/main/java/tls.java
+++ src/main/java/tls.java
@@ -8,25 +8,32 @@ public class tls {
 
         // Only 1 argument, prints to command line if valid path
         if (args.length == 1){
+            String path = args[0];
 
         }
 
         // 2 arguments and first argument is "-o", output to .csv file in same directory
         else if (args.length == 2 && args[0]=="-o"){
+            String path = args[1];
 
         }
 
         // 3 arguments and first argument is "-o", output to .csv file in specified directory
         else if (args.length == 3 && args[0]=="-o"){
+            String path = args[2];
 
         }
 
         // Invalid argument
         else {
-            System.err.println("Usage correcte: java <chemin-de-l'entree> || java -o <chemin-sortie> <chemin-entree>");
+            System.err.println("Usage correcte: java <chemin-entree> || java -o (<chemin-sortie>) <chemin-entree>");
             System.exit(1);
         }
 
+    }
+
+    // Find every test java file within the directory and its sub-directories
+    public static List<File> testFiles(String path){
 
     }
 
