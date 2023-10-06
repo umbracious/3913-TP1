fa889e4ab8bfb30bd7774604d34a5d11bd78b30b
umbracious <63139410+umbracious@users.noreply.github.com>
2023-10-06 18:21:18 -0400

    finished csv file output

--- src/main/java/tropcomp.java
+++ src/main/java/tropcomp.java
@@ -1,4 +1,6 @@
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.*;
 
 public class tropcomp {
@@ -64,16 +66,160 @@ public class tropcomp {
             }
         }
 
-        else if (args.length==3){
-            return;
+        else if (args.length==3 && args[0]=="-o"){
+
+            float threshold = Float.parseFloat(args[2]);
+            String path = args[1];
+            File directory = new File(path);
+
+            // Invalid directory argument
+            if (!directory.exists() || !directory.isDirectory()){
+                System.err.println("Incorrect directory: java <entry-dir>");
+                System.exit(1);
+            }
+
+            // Find all test files within directory
+            List<File> testFiles = tls.findTestFiles(directory);
+
+            List<Integer> tlocList = new ArrayList<>();
+            List<Float> tcmpList = new ArrayList<>();
+
+            // Compile a list of tloc and tcmp
+            for (File file: testFiles) {
+                HashMap info = tls.getInfo(file, path);
+                int currTLOC = (int) info.get("tloc");
+                int currTASSERT = (int) info.get("tassert");
+                float currTCMP = currTLOC/currTASSERT;
+
+                tlocList.add(currTLOC);
+                tcmpList.add(currTCMP);
+
+            }
+
+            // Sort both lists
+            Collections.sort(tlocList);
+            Collections.sort(tcmpList);
+
+            int tlocListSize = tlocList.size();
+            int tcmpListSize = tcmpList.size();
+
+            // Output to tls.csv
+            try {
+
+                FileWriter fileWriter = new FileWriter("tls.csv");
+
+                // Revisit each file to calculate its percentile and compare it to the threshold
+                for (File file: testFiles){
+
+                    HashMap info = tls.getInfo(file, path);
+                    int currTLOC = (int) info.get("tloc");
+                    int currTASSERT = (int) info.get("tassert");
+                    float currTCMP = currTLOC/currTASSERT;
+
+                    float tlocPercentile = (tlocList.indexOf(currTLOC)+1)/tlocListSize;
+                    float tcmpPercentile = (tcmpList.indexOf(currTCMP)+1)/tcmpListSize;
+
+                    // If both percentiles exceed the threshold, write the files info to the .csv file
+                    if (tlocPercentile >= (100 - threshold) && tcmpPercentile >= (100 - threshold)){
+                        // Write info from hashmap to .csv file
+                        fileWriter.write(info.get("relativePath") + "," + info.get("package") + ","
+                                + info.get("className") + "," + Integer.toString((Integer) info.get("tloc")) + ","
+                                + Integer.toString((Integer) info.get("tassert"))  + ","
+                                + Float.toString((Float) info.get("tcmp")) + "\n");
+                    }
+
+
+
+                }
+
+                fileWriter.close();
+
+            } catch (IOException e) {
+                System.out.println("Invalid output file directory");
+                e.printStackTrace();
+                System.exit(1);
+            }
+
         }
 
-        else if (args.length==4){
-            return;
+        else if (args.length==4 && args[0] == "-o"){
+
+            float threshold = Float.parseFloat(args[3]);
+            String path = args[2];
+            File directory = new File(path);
+
+            // Invalid directory argument
+            if (!directory.exists() || !directory.isDirectory()){
+                System.err.println("Incorrect directory: java <entry-dir>");
+                System.exit(1);
+            }
+
+            // Find all test files within directory
+            List<File> testFiles = tls.findTestFiles(directory);
+
+            List<Integer> tlocList = new ArrayList<>();
+            List<Float> tcmpList = new ArrayList<>();
+
+            // Compile a list of tloc and tcmp
+            for (File file: testFiles) {
+                HashMap info = tls.getInfo(file, path);
+                int currTLOC = (int) info.get("tloc");
+                int currTASSERT = (int) info.get("tassert");
+                float currTCMP = currTLOC/currTASSERT;
+
+                tlocList.add(currTLOC);
+                tcmpList.add(currTCMP);
+
+            }
+
+            // Sort both lists
+            Collections.sort(tlocList);
+            Collections.sort(tcmpList);
+
+            int tlocListSize = tlocList.size();
+            int tcmpListSize = tcmpList.size();
+
+            // Output to tls.csv
+            try {
+
+                FileWriter fileWriter = new FileWriter(args[1]);
+
+                // Revisit each file to calculate its percentile and compare it to the threshold
+                for (File file: testFiles){
+
+                    HashMap info = tls.getInfo(file, path);
+                    int currTLOC = (int) info.get("tloc");
+                    int currTASSERT = (int) info.get("tassert");
+                    float currTCMP = currTLOC/currTASSERT;
+
+                    float tlocPercentile = (tlocList.indexOf(currTLOC)+1)/tlocListSize;
+                    float tcmpPercentile = (tcmpList.indexOf(currTCMP)+1)/tcmpListSize;
+
+                    // If both percentiles exceed the threshold, write the files info to the .csv file
+                    if (tlocPercentile >= (100 - threshold) && tcmpPercentile >= (100 - threshold)){
+                        // Write info from hashmap to .csv file
+                        fileWriter.write(info.get("relativePath") + "," + info.get("package") + ","
+                                + info.get("className") + "," + Integer.toString((Integer) info.get("tloc")) + ","
+                                + Integer.toString((Integer) info.get("tassert"))  + ","
+                                + Float.toString((Float) info.get("tcmp")) + "\n");
+                    }
+
+
+
+                }
+
+                fileWriter.close();
+
+            } catch (IOException e) {
+                System.out.println("Invalid output file directory");
+                e.printStackTrace();
+                System.exit(1);
+            }
+
         }
 
         else {
-            System.out.println("Invalid Arguments. Correct syntax: java tropcomp (-o (<output-dir>) <input-dir>" +
+            System.out.println("Invalid Arguments. Correct syntax: java tropcomp (-o (<output-dir>)) <input-dir>" +
                     " <threshold>");
         }
 
